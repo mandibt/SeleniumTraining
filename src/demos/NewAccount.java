7b6f0c912dd626e482d2f45a2e84ff2ff4096cb4
mandibt <34835964+mandibt@users.noreply.github.com>
2018-01-04 17:01:39 +0100

    created webelements intest checkbox algorithm

--- src/demos/NewAccount.java
+++ src/demos/NewAccount.java
@@ -2,8 +2,7 @@ package demos;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class NewAccount {
@@ -15,37 +14,90 @@ public class NewAccount {
 		String country = "Brazil";
 		String phoneNumber = "1231231234";
 		String browser = "";
-		WebDriver driver; 
-		String gender;
-		String weeklyEmail;
-		String monthlyEmail;
-		String occasionalEmail; 
-
+		String gender = "fEMALE";
+		boolean weeklyEmail = true;
+		boolean monthlyEmail = true;
+		boolean occasionalEmail = false; 
 		
-		//1. Open browser 
-		driver = utilities.driverFactory.openBrowser(browser);
+		//1. Define WebDriver
+		WebDriver driver = utilities.driverFactory.openBrowser(browser);
+
 		
 		//2. Visit the page and click on Create Account 
 		driver.manage().window().maximize();
 		driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");	
-		driver.findElement(By.xpath("//*[@id='ctl01']/div[3]/div[2]/div/div[2]/a")).click(); ;
+		
+		WebElement createAccount	= driver.findElement(By.xpath("//*[@id='ctl01']/div[3]/div[2]/div/div[2]/a"));
+		createAccount.click(); ;
 		Thread.sleep(2000);
 		
 		//3. Fill out the form
-		driver.findElement(By.id("MainContent_txtFirstName")).sendKeys(name);
-		driver.findElement(By.name("ctl00$MainContent$txtEmail")).sendKeys(email);
-		driver.findElement(By.xpath("//*[@id=\'MainContent_txtHomePhone\']")).sendKeys(phoneNumber);
-		driver.findElement(By.cssSelector("input[id='MainContent_txtPassword']")).sendKeys(password);
-		driver.findElement(By.id("MainContent_txtVerifyPassword")).sendKeys(password);
-		driver.findElement(By.cssSelector("input[name='ctl00$MainContent$Gender'][value='Female']")).click();
-		new Select(driver.findElement(By.id("MainContent_menuCountry"))).selectByVisibleText(country); ;
-		driver.findElement(By.id("MainContent_checkWeeklyEmail")).click();
+		WebElement firstName 	 	= driver.findElement(By.id("MainContent_txtFirstName"));
+		WebElement eMail		 	= driver.findElement(By.name("ctl00$MainContent$txtEmail"));
+		WebElement phoneNo		 	= driver.findElement(By.id("MainContent_txtHomePhone"));
+		WebElement passWord 	 	= driver.findElement(By.cssSelector("input[id='MainContent_txtPassword']"));
+		WebElement verifyPassword   = driver.findElement(By.id("MainContent_txtVerifyPassword"));
+		WebElement femaleRadio		= driver.findElement(By.id("MainContent_Female"));
+		WebElement maleRadio		= driver.findElement(By.id("MainContent_Male"));
+		WebElement selectCountry	= driver.findElement(By.id("MainContent_menuCountry"));
+		WebElement weeklychkbox     = driver.findElement(By.id("MainContent_checkWeeklyEmail"));
+		WebElement monthlychkbox     = driver.findElement(By.id("MainContent_checkMonthlyEmail"));
+		WebElement ocasschkbox  	= driver.findElement(By.id("MainContent_checkUpdates"));
+		WebElement submitButton		= driver.findElement(By.id("MainContent_btnSubmit"));
+		
+		firstName.sendKeys(name);
+		eMail.sendKeys(email);
+		phoneNo.sendKeys(phoneNumber);
+		passWord.sendKeys(password);
+		verifyPassword.sendKeys(password);
+		// Radio buttons
+		if (gender.equalsIgnoreCase("female")) { femaleRadio.click(); }
+		else { maleRadio.click(); }
+		//Country selection
+		new Select(selectCountry).selectByVisibleText(country); ;
+		
+		// Checkbox algorithm
+		if (weeklyEmail) {
+			if (!weeklychkbox.isSelected()) {
+					weeklychkbox.click();
+			}
+		}
+		else {
+			if (weeklychkbox.isSelected()) {
+				weeklychkbox.click();
+			}
+		}
+		if (monthlyEmail) {
+			if (!monthlychkbox.isSelected()) {
+					monthlychkbox.click();
+			}
+		}
+		else {
+			if (monthlychkbox.isSelected()) {
+				monthlychkbox.click();
+			}
+		}
+		if (occasionalEmail) {
+			if (!ocasschkbox.isSelected()) {
+					ocasschkbox.click();
+			}
+		}
+		else {
+			if (ocasschkbox.isSelected()) {
+				ocasschkbox.click();
+			}
+		}
 		Thread.sleep(2000);
-		driver.findElement(By.id("MainContent_btnSubmit")).click();
+		//submitButton.click();
 		
 		//4. Validate confirmation
 		String conf = driver.findElement(By.id("MainContent_lblTransactionResult")).getText();
-		System.out.println("Confrimation: "+conf); 
+		String expected = "Customer information added successfully";
+		if (conf.contains(expected)) 
+			System.out.println("Confrimation: "+conf); 
+		else
+			System.out.println("Test Failed");
+		Thread.sleep(2000);
 		
 		//5. Close 
 		driver.close();
